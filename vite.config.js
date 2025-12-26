119e2c33a4713702c64741110ae83b7a1d386aa6
Eli Finer <eli.finer@gmail.com>
2025-12-26 17:33:34 +0200

    Add Vite plugin to copy _redirects file to dist for Cloudflare Pages SPA routing

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { copyFileSync } from 'fs'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        copyFileSync(
+          resolve(__dirname, '_redirects'),
+          resolve(__dirname, 'dist', '_redirects')
+        )
+      }
+    }
+  ],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
   },
 })
-
